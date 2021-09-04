bb7e089e84a2d2692bee242233a8a030c2c02a53
KanoplichAndrey <kanop0000@gmail.com>
2021-09-05 02:32:29 +0300

    fffddf

---
+++ menu.js
@@ -0,0 +1,35 @@
+let menuElem = document.getElementById('sweeties');
+let titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function() {
+menuElem.classList.toggle('open');
+};
+
+let menuElem2 = document.getElementById('sweeties2');
+let titleElem2 = menuElem2.querySelector('.title');
+
+titleElem2.onclick = function() {
+menuElem2.classList.toggle('open');
+};
+
+let menuElem3 = document.getElementById('sweeties3');
+let titleElem3 = menuElem3.querySelector('.title');
+
+titleElem3.onclick = function() {
+menuElem3.classList.toggle('open');
+};
+
+let menuElem4 = document.getElementById('sweeties4');
+let titleElem4 = menuElem4.querySelector('.title');
+
+titleElem4.onclick = function() {
+menuElem4.classList.toggle('open');
+};
+
+let menuElem5 = document.getElementById('sweeties5');
+let titleElem5 = menuElem5.querySelector('.title');
+
+titleElem5.onclick = function() {
+menuElem5.classList.toggle('open');
+};
+
